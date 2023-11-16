7540392857b2305d4b146b8d401b005135adc0ee
Naihl <bonnelnoah@gmail.com>
2023-11-16 10:14:49 +0100

    Update hangman

--- v0/showHangman.c
+++ v0/showHangman.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
+// Function to display the hangman based on the number of errors
 void showHangman(int errors) {
-
+    
+    // Check if the number of errors is outside the valid range
     if (errors < 0 || errors > 6) {
-        perror("Erreur : Le nombre d'erreurs doit être compris entre 0 et 6");
+        perror("Error: Number must be between 0 and 6");
         return;
     }
 
+    // Display the basic structure of the hangman
     printf(" +---+\n");
     printf(" |   |\n");
 
+    // Display the hangman's head if at least 1 error
     if (errors >= 1) {
         printf(" |   O\n");
     } else {
         printf(" |    \n");
     }
 
+    // Display the left arm if 2 errors, the right arm if there are 3 errors,
+    // and both arms if there are 4 errors or more
     if (errors == 2) {
         printf(" |   |\n");
     } else if (errors == 3) {
         printf(" |  /| \n");
-    
     }
     else if (errors >= 4) {
         printf(" |  /|\\\n");
@@ -28,6 +33,7 @@ void showHangman(int errors) {
         printf(" |    \n");
     }
 
+    // Display the left leg if 5 errors, both legs if there are 6 errors or more
     if (errors == 5) {
         printf(" |  /\n");
     } else if (errors >= 6) {
@@ -36,8 +42,8 @@ void showHangman(int errors) {
         printf(" |    \n");
     }
 
+    // Display the base of the hangman
     printf(" |_____\n");
-
 }
 
 int main() {
